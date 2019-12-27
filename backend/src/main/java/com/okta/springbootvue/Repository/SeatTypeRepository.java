6446437f4d1b7103d24660b229429631b8b285ad
amarinT <amarin.ploy@gmail.com>
2019-12-27 12:02:55 +0700

    add Repository ระบบจองตั๋วภาพยนตร์ - close #55

---
+++ backend/src/main/java/com/okta/springbootvue/Repository/SeatTypeRepository.java
@@ -0,0 +1,12 @@
+package com.okta.springbootvue.Repository;
+
+import com.okta.springbootvue.Entity.SeatType;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public
+interface SeatTypeRepository extends JpaRepository<SeatType, Long> {
+	SeatType findById(long id);
+}
