c752479f3ed6052c413e08b2c159167c3624f3e5
SaranSmr <earnfang@gmail.com>
2019-12-27 18:10:02 +0700

    add file Entity Food - close #49

---
+++ backend/src/main/java/com/okta/springbootvue/Entity/Food.java
@@ -0,0 +1,35 @@
+package com.okta.springbootvue.Entity;
+
+import lombok.*;
+
+import javax.persistence.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.FetchType;
+
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name="FOOD")
+public class Food {
+	@Id
+	@SequenceGenerator(name="food_seq",sequenceName="food_seq")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="food_seq")
+	@Column(name="FOOD_ID",unique = true, nullable = true)
+	private @NonNull Long id;
+	private @NonNull String name;
+
+	@OneToMany(fetch = FetchType.EAGER)
+	// mappedBy  = "rentCustomer"
+	private Collection<Foodanddrink> buy;
+}
