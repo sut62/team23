0713eb499dba22a598a045e0cfabedc94a0eb737
SaranSmr <earnfang@gmail.com>
2019-12-27 18:27:12 +0700

    add file Controller FoodController - close #30

---
+++ backend/src/main/java/com/okta/springbootvue/Controller/FoodController.java
@@ -0,0 +1,39 @@
+package com.okta.springbootvue.Controller;
+
+import com.okta.springbootvue.Entity.Food;
+import com.okta.springbootvue.Repository.FoodRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+public class FoodController {
+
+    @Autowired
+    private final FoodRepository foodRepository;
+
+    public FoodController(FoodRepository foodRepository) {
+        this.foodRepository = foodRepository;
+    }
+
+    @GetMapping("/food")
+    public Collection<Food> food() {
+        return foodRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+    /*@GetMapping("/food/{id}")
+    public Optional<Food> Customers(@PathVariable Long id) {
+        Optional<Customer> customer = customerRepository.findById(id);
+        return customer;
+    }*/
+
+}
