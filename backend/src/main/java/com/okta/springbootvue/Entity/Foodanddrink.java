b94d2c1ebf58f5231c4327e9ea9dc9485d1abc32
SaranSmr <earnfang@gmail.com>
2019-12-27 18:09:04 +0700

    add file Entity Foodanddrink - close #49

---
+++ backend/src/main/java/com/okta/springbootvue/Entity/Foodanddrink.java
@@ -0,0 +1,48 @@
+package com.okta.springbootvue.Entity;
+
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+
+import com.okta.springbootvue.Entity.Employee;;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name="FoodAndDrink")
+public class Foodanddrink {
+
+    @Id
+    @SequenceGenerator(name="FoodAndDrink_seq",sequenceName="FoodAndDrink_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="FoodAndDrink_seq")
+    @Column(name = "FoodAndDrink_ID", unique = true, nullable = true)
+    private @NonNull Long id;
+
+    @Column(name="BUY_DATE")
+    private @NonNull Date buyDate;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Employee.class)
+    @JoinColumn(name = "EMPLOYEE_ID", insertable = true)
+    private Employee employee;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Drink.class)
+    @JoinColumn(name = "DRINK_ID", insertable = true)
+    private Drink drink;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Food.class)
+    @JoinColumn(name = "FOOD_ID", insertable = true)
+    private Food food;
+}
