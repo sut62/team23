0c0f3cb78acadc8a3715a5095cbe072585b5f9a1
SaranSmr <earnfang@gmail.com>
2019-12-27 19:06:24 +0700

    add file Repository FoodanddrinkRepository - close #56

---
+++ backend/src/main/java/com/okta/springbootvue/Repository/FoodanddrinkRepository.java
@@ -0,0 +1,11 @@
+package com.okta.springbootvue.Repository;
+
+import com.okta.springbootvue.Entity.Foodanddrink;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public
+interface FoodanddrinkRepository extends JpaRepository<Foodanddrink, Long> {
+}
