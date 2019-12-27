619dea4155a0cb63703b4fa99b49e27f63bc35a0
kamsin123 <kamsin6@gmail.com>
2019-12-27 16:25:04 +0700

    add Entity Room - close #46

---
+++ backend/src/main/java/com/okta/springbootvue/Entity/Room.java
@@ -0,0 +1,31 @@
+package com.okta.springbootvue.Entity;
+import lombok.*;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.util.Collection;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name="Room")
+public class Room {
+	@Id
+	@SequenceGenerator(name="room_seq",sequenceName="room_seq")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="room_seq")
+	@Column(name="Room_ID",unique = true, nullable = true)
+	private @NonNull Long idR;
+	private @NonNull String room;
+
+	@OneToMany(fetch = FetchType.EAGER)
+	private Collection<ManageMovie> theater;
+
+
+
+}
