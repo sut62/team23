488a2cfc7b0a75310a24d3f7ec44dd47e03c93fe
Mui2108 <mooy.panupong21@gmail.com>
2020-01-07 20:20:28 +0700

    Add file CountryController - close #26

---
+++ backend/src/main/java/com/okta/springbootvue/Controller/CountryController.java
@@ -0,0 +1,39 @@
+package com.okta.springbootvue.Controller;
+
+
+
+import com.okta.springbootvue.Entity.Country;
+import com.okta.springbootvue.Repository.CountryRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+public class CountryController {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    public CountryController(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @GetMapping("/country")
+    public Collection<Country> country() {
+        return countryRepository.findAll().stream().collect(Collectors.toList());
+    }
+    
+}
