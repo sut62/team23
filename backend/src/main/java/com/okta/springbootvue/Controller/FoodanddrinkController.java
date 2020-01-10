8c672e88f5d56fb149e04233cd1d79c90f20c06c
SaranSmr <earnfang@gmail.com>
2020-01-10 14:52:10 +0700

    add file update Controller FoodanddrinkController - close #76

--- backend/src/main/java/com/okta/springbootvue/Controller/FoodanddrinkController.java
+++ backend/src/main/java/com/okta/springbootvue/Controller/FoodanddrinkController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.io.IOException;
 import java.sql.Timestamp;
@@ -24,48 +25,56 @@ import com.okta.springbootvue.Entity.Food;
 import com.okta.springbootvue.Entity.Drink;
 import com.okta.springbootvue.Entity.Employee;
 import com.okta.springbootvue.Entity.Foodanddrink;
+import com.okta.springbootvue.Entity.User;
 import com.okta.springbootvue.Repository.FoodRepository;
 import com.okta.springbootvue.Repository.DrinkRepository;
 import com.okta.springbootvue.Repository.EmployeeRepository;
 import com.okta.springbootvue.Repository.FoodanddrinkRepository;
+import com.okta.springbootvue.Repository.UserRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class FoodanddrinkController {
     @Autowired
-    private final FoodanddrinkRepository foodanddrinkRepository;
+    private FoodanddrinkRepository foodanddrinkRepository;
     @Autowired
     private FoodRepository foodRepository;
     @Autowired
     private DrinkRepository drinkRepository;
     @Autowired
     private EmployeeRepository employeeRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     FoodanddrinkController(FoodanddrinkRepository foodanddrinkRepository) {
         this.foodanddrinkRepository = foodanddrinkRepository;
     }
 
     @GetMapping("/foodanddrink")
-    public Collection<Foodanddrink> foodanddrink() {
+    public Collection<Foodanddrink> foodanddrinks() {
         return foodanddrinkRepository.findAll().stream().collect(Collectors.toList());
     }
 
-    @PostMapping("/videoRental/{customer_id}/{employee_id}/{video_id}/{rentalType}")
+    @PostMapping("/Foodanddrink/{user_id}/{food_id}/{drink_id}/{employee_id}")
     public Foodanddrink newFoodanddrink(Foodanddrink newFoodanddrink,
     @PathVariable long food_id,
     @PathVariable long drink_id,
-    @PathVariable long employee_id) {
+    @PathVariable long employee_id,
+    @PathVariable long user_id) {
     //VideoRental newVideoRental = new VideoRental();
 
     Food food = foodRepository.findById(food_id);
     Drink drink = drinkRepository.findById(drink_id);
     Employee employee = employeeRepository.findById(employee_id);
+    User user = userRepository.findById(user_id);
 
     newFoodanddrink.setFood(food);
     newFoodanddrink.setDrink(drink);
     //newFoodanddrink.setRentDate(new Date());
     newFoodanddrink.setEmployee(employee);
+    newFoodanddrink.setUser(user);
+
 
     return foodanddrinkRepository.save(newFoodanddrink); //บันทึก Objcet ชื่อ Foodanddrink
     
