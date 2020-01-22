38ee94cffd71deaa6846f4bbc9a1f02beb047d91
Mui2108 <mooy.panupong21@gmail.com>
2020-01-22 19:18:50 +0700

    แก้ไข Gender Entity - close #102

--- backend/src/main/java/com/okta/springbootvue/Entity/Gender.java
+++ backend/src/main/java/com/okta/springbootvue/Entity/Gender.java
@@ -7,13 +7,21 @@ import javax.persistence.GenerationType;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
+
 @Entity
 public class Gender{
     @Id
     @SequenceGenerator(name="genseq",sequenceName="genseq") 
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="genseq")  
     private Long gender_id;
-    private @NonNull String gender;
+
+    
+    @NotNull 
+    @Size(min = 1,max = 5,message = "size must be between 1 and 5")
+    private String gender_name;
 
     public void setGender_id(Long gender_id){
         this.gender_id= gender_id;
@@ -22,11 +30,11 @@ public class Gender{
         return gender_id;
     }
 
-    public void setGender(String gender){
-        this.gender = gender;
+    public void setGender_name(String gender_name){
+        this.gender_name = gender_name;
     }
-    public String getGender(){
-        return gender;
+    public String getGender_name(){
+        return gender_name;
     }
     
 }
