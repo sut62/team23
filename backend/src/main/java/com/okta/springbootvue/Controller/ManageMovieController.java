da02cf5e72b083111fb7dc0f941adab5a2bb533c
kamsin123 <kamsin6@gmail.com>
2020-01-26 00:44:00 +0700

    แก้ UI ระบบจัดการรอบฉายภาพยนตร์ - close #119

--- backend/src/main/java/com/okta/springbootvue/Controller/ManageMovieController.java
+++ backend/src/main/java/com/okta/springbootvue/Controller/ManageMovieController.java
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
@@ -51,14 +50,25 @@ public class ManageMovieController {
     public List<ManageMovie> ManageMovies() {
         return manageMovieRepository.findAll().stream().collect(Collectors.toList());
     }
+    /*@GetMapping("/managemovie/checkData/{room_id}/{movie_time}/{movie_day}")
+    Collection<ManageMovie> findByData(@PathVariable(" room_id") long room_id, @PathVariable("movie_time") long movie_time, @PathVariable("date") Date movie_day)
+    {
+        return manageMovieRepository.findByData(room_id, movie_time, movie_day);
+    }
+    */
+    @PostMapping("/managemovie/{movie_id}/{dub_id}/{room_id}/{movie_time}/{note}/{movie_day}")
+    public ManageMovie newManageMovie(ManageMovie newManageMovie, 
+                                    @PathVariable long movie_id,
+                                    @PathVariable long dub_id, 
+                                    @PathVariable long room_id,
+                                    @PathVariable long movie_time ,
+                                    @PathVariable String note, 
+                                    @PathVariable String movie_day ) {
 
-    @PostMapping("/managemovie/{id}/{dub_id}/{room_id}/{movie_time}/{note}/{movie_day}")
-    public ManageMovie newManageMovie(ManageMovie newManageMovie, @PathVariable long id,
-            @PathVariable long dub_id, @PathVariable long room_id,@PathVariable long movie_time , @PathVariable String note, @PathVariable String movie_day ) {
-
-        MovieAdd movieAdd = movieaddRepository.findById(id);
+        MovieAdd movieAdd = movieaddRepository.findById(movie_id);
         Dub lan = dubRepository.findById(dub_id);
         Room ro = roomRepository.findById(room_id);
+
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
             try {
         Date date = formatter.parse(movie_day);
