82b0218003f5e0de77ecd2f5dd9e998471e7016e
subancha <subancha.thep@gmail.com>
2020-01-26 04:44:28 +0700

    เพิ่มไฟล์ MoviePics - close #129

---
+++ client/src/components/MoviePics.vue
@@ -0,0 +1,24 @@
+<template>
+        <v-carousel cycle
+    height="650"
+    hide-delimiter-background
+    show-arrows-on-hover>
+          <v-carousel-item key="1" src="http://www.impawards.com/2019/posters/avengers_endgame_ver3_xlg.jpg">
+            
+          </v-carousel-item>
+          <v-carousel-item key="2" src="http://www.impawards.com/2018/posters/aquaman_ver14_xlg.jpg">
+            
+          </v-carousel-item>
+          <v-carousel-item key="3" src="http://www.impawards.com/2017/posters/star_wars_the_last_jedi_ver63_xlg.jpg">
+          </v-carousel-item>
+
+          <v-carousel-item key="4" src="http://warofthemovies.com/wp-content/uploads/2015/01/the-imitation-gamel7P91mJNnTdsoLEtefBI.jpg">
+          </v-carousel-item>
+
+          <v-carousel-item key="5" src="http://www.impawards.com/2019/posters/frozen_two_ver31_xlg.jpg">
+          </v-carousel-item>
+
+          <v-carousel-item key="5" src="http://www.impawards.com/2017/posters/dunkirk_ver10_xlg.jpg">
+          </v-carousel-item>
+        </v-carousel>
+      </template>
