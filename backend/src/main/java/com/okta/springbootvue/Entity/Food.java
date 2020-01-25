9f4090cf5c7f4a981d070c32afff62ecbf9d1d3a
SaranSmr <earnfang@gmail.com>
2020-01-26 05:46:48 +0700

    add file Entity Food - close #136

--- backend/src/main/java/com/okta/springbootvue/Entity/Food.java
+++ backend/src/main/java/com/okta/springbootvue/Entity/Food.java
@@ -11,9 +11,10 @@ import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-
-import java.util.Collection;
-
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.util.Collection; 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
@@ -26,9 +27,17 @@ public class Food {
 	@SequenceGenerator(name="food_seq",sequenceName="food_seq")
     @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="food_seq")
 	@Column(name="FOOD_ID",unique = true, nullable = true)
-	private @NonNull Long id;
-	private @NonNull String name;
-	private @NonNull int price;
+	@NotNull
+	private Long id;
+
+
+	@NotNull
+	private String name;
+
+	@NotNull
+	@Max(value = 200, message = "Food Price should not be greater than 200")
+	@Min(value = 30, message = "Food Price should not be less than 30")
+	private int price;
 
 	@OneToMany(fetch = FetchType.EAGER)
 	// mappedBy  = "rentCustomer"
