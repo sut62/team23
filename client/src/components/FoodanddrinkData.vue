f147264a861ca8fbcdae1be1479744fd63eb978c
SaranSmr <earnfang@gmail.com>
2020-01-26 06:21:38 +0700

    add file FoodanddrinkData - close #97

---
+++ client/src/components/FoodanddrinkData.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-container>
+    <v-layout text-center wrap>
+      <v-flex mb-4>
+        <br />
+        <h1 class="display-2 font-weight-bold mb-3">Food and Drink Information</h1>
+      </v-flex>
+    </v-layout>
+
+    <v-row justify="center">
+      <v-col cols="8">
+        <v-data-table :headers="headers" :items="items" :items-per-page="5" class="elevation-1">
+        </v-data-table>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import http from "../http-common";
+
+export default {
+  name: "FoodanddrinkData",
+  data() {
+    return {
+      headers: [
+        {
+          text: "User",
+           align: "left",
+           sortable: false,
+          value: "user.name"
+        },
+        { text: "Food", value: "food.name" },
+        { text: "Drink", value: "drink.name" },
+        { text: "Employee", value: "employee.employee" },
+        //{ text: "LeaveDate", value: "leave" },
+        { text: "Note", value: "note" }
+
+      ],
+      items: []
+    };
+  },
+  methods: {
+    /* eslint-disable no-console */
+    // ดึงข้อมูล VideoRental ใส่ combobox
+   getFoodanddrinks() {
+      http
+        .get("/foodanddrink")
+        .then(response => {
+          this.items = response.data;
+          console.log(this.items);
+        })
+        .catch(e => {
+        console.log(e);
+        });
+    },
+    refreshList() {
+      this.getFoodanddrinks();
+    }
+    /* eslint-enable no-console */
+  },
+  mounted() {
+    this.getFoodanddrinks();
+  }
+};
+</script>
