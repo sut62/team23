cd70f2618a9452a6af572979939bd25b8b3514e8
kamsin123 <kamsin6@gmail.com>
2020-02-08 14:27:22 +0700

    อัพเดท Entity Managemovie - close #172

--- backend/src/main/java/com/okta/springbootvue/Entity/ManageMovie.java
+++ backend/src/main/java/com/okta/springbootvue/Entity/ManageMovie.java
@@ -7,6 +7,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -45,9 +46,10 @@ public class ManageMovie {
     @ManyToOne(fetch = FetchType.EAGER, targetEntity = Room.class)
     @JoinColumn(name = "ROOM_ID", insertable = true)
     private Room ro;
-    
+
+    @NotNull
     @Temporal(TemporalType.DATE)
-    private Date date;
+    private  Date date;
 
     @ManyToOne(fetch = FetchType.EAGER, targetEntity = MovieTime.class)
     @JoinColumn(name = "Time_ID", insertable = true)
