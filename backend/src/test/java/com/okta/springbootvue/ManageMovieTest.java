0e316218720b77becdd85ec6e00a886900577b08
kamsin123 <kamsin6@gmail.com>
2020-02-08 14:29:37 +0700

    แก้ TestCase ManageMoiveTest - close #172

--- backend/src/test/java/com/okta/springbootvue/ManageMovieTest.java
+++ backend/src/test/java/com/okta/springbootvue/ManageMovieTest.java
@@ -8,10 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Optional;
 import java.util.Set;
 
@@ -33,20 +37,48 @@ public class ManageMovieTest {
     }
 
     // ตั้งชื่อ test ให้สอดคล้องกับสิ่งที่ต้อง test
+    /*@Test
+    void B6011987_TestDateisCorrect(){
+        ManageMovie movie = new ManageMovie();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd") ;
+        try {
+            Date date = formatter.parse("2020-01-13");
+            movie.setDate(date) ;
+        } catch (ParseException e) {}
+        
+        movie = manageMovieRepository.saveAndFlush(movie);
+
+        Optional<ManageMovie> found = manageMovieRepository.findById(movie.getId());
+        try {
+            assertEquals(formatter.parse("2020-01-13"), found.get().getDate());
+        } catch (ParseException e) {}
+    }*/
     @Test
     void B6011987_testManageMoiveNormal() {
         ManageMovie movie = new ManageMovie();
         movie.setNote("kidmaioog");
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd") ;
+        try {
+            Date date = formatter.parse("2020-01-13");
+            movie.setDate(date) ;
+        } catch (ParseException e) {}
         movie = manageMovieRepository.saveAndFlush(movie);
-
-
         Optional<ManageMovie> found = manageMovieRepository.findById(movie.getId());
         assertEquals("kidmaioog", found.get().getNote());
+        try {
+            assertEquals(formatter.parse("2020-01-13"), found.get().getDate());
+        } catch (ParseException e) {}
     }
+
     @Test
     void B6011987_testNoteIsMoreThanSize() {
         ManageMovie movie = new ManageMovie();
         movie.setNote("1234567890123456789012345678901234567890312156451");
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd") ;
+        try {
+            Date date = formatter.parse("2020-01-13");
+            movie.setDate(date) ;
+        } catch (ParseException e) {}
 
         Set<ConstraintViolation<ManageMovie>> result = validator.validate(movie);
 
@@ -57,6 +89,25 @@ public class ManageMovieTest {
         ConstraintViolation<ManageMovie> c = result.iterator().next();
         assertEquals("must be less than or equal 30 characters", c.getMessage());
         assertEquals("note", c.getPropertyPath().toString());
+    }
+    @Test
+    void B6011987_testDateMustNotBeNull() {
+        ManageMovie movie = new ManageMovie();
+        movie.setNote("kidmaioog");
+        try {
+            movie.setDate(null);
+            //movie = manageMovieRepository.saveAndFlush(movie);
+        } catch (ConstraintViolationException e) {}
+
+        Set<ConstraintViolation<ManageMovie>> result = validator.validate(movie);
+
+        // result ต้องมี error 1 ค่าเท่านั้น
+        assertEquals(1, result.size());
+
+        // error message ตรงชนิด และถูก field
+        ConstraintViolation<ManageMovie> d = result.iterator().next();
+        assertEquals("must not be null", d.getMessage());
+        assertEquals("date", d.getPropertyPath().toString());
     }
 
     
